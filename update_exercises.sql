157a74d005c5f8ff392d3c59e2c498ae6e7bf0b5
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-04-18 15:57:11 -0500

    last min fix

--- update_exercises.sql
+++ update_exercises.sql
@@ -3,7 +3,7 @@ USE codeup_test_db;
     UPDATE albums SET sales = sales * '10';
 
     SELECT name AS 'Albums Before 1980' FROM albums WHERE release_date < '1980';
-    UPDATE albums SET release_date - '100' WHERE release_date < '1980';
+    UPDATE albums SET release_date = release_date - '100' WHERE release_date < '1980';
 
     SELECT name AS 'Albums by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
-    UPDATE albums SET 'Peter Jackson' WHERE artist = 'Michael Jackson';
+    UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
