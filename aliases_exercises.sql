aa43619df1f523ae85d76fc2b40b3de6c1680ddf
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-04-20 15:04:10 -0500

    all done with aliases

---
+++ aliases_exercises.sql
@@ -0,0 +1,12 @@
+USE employees;
+DESCRIBE employees;
+
+#3
+SELECT CONCAT(last_name,', ', first_name) AS full_name
+FROM employees
+LIMIT 10;
+
+#4, 5
+SELECT CONCAT(emp_no, ' - ',last_name,', ', first_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10;
