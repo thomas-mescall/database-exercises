4508fa9b6de9fa47bfeabcf22653f426b1193221
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-04-20 09:36:34 -0500

    all done with limit

---
+++ limit_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+
+DESCRIBE employees;
+
+DESCRIBE salaries;
+
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
+
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 45;
